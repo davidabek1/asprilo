8d2956e761588332704129869fa17fd8cfffc017
zeged85 <zeged@yahoo.com>
2019-06-26 18:52:09 -0700

    added support for assigning robots to shelves

--- generate_problems.py
+++ generate_problems.py
@@ -6,3 +6,4 @@ os.chdir('/home/david/asprilo/')
 for k in range(5, 80, 5):
     #os.popen('gen -x 15 -y 12 -r {0} -s {0} -N 5 --prs 1 --spr 1 --pus 1 --ol 1 -p 1 -P {0} -u {0} -o {0}'.format(k))
     subprocess.call('gen -x 15 -y 12 -r {0} -s {0} -N 5 --prs 1 --spr 1 --pus 1 --ol 1 -p 1 -P {0} -u {0} -o {0}'.format(k), shell=True)
+    subprocess.call('python ./fix_asprilo_assignment.py generatedInstances/x15_y12_n180_r{0}_s{0}_ps1_pr{0}_u{0}_o{0}_l{0}_N{1:03d}.lp'.format(k,i+1), shell=True)
